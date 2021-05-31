30cc3e24adcc4bc903076aaf8213a8c7ef3eccf1
Danilo <dankusanovic@gmail.com>
2021-05-30 17:26:11 -0700

    New Elastic1DGap Material was added and implemented

---
+++ 02-Run_Process/02-Materials/02-NonLinear/Elastic1DGap.cpp
@@ -0,0 +1,162 @@
+#include <cfloat>
+#include "Elastic1DGap.hpp" 
+#include "Definitions.hpp"
+
+//Define constant tolerance value:
+const double TOL = 1.0E-06;
+
+//Overload constructor.
+Elastic1DGap::Elastic1DGap(double E, double gap, bool behavior) : 
+Material("Elastic1DGap", false), E(E), Behavior(behavior){  
+    //Initialize fiber internal variables.
+    Gap = fabs(gap),
+	oldStrain = 0.0;
+}
+
+//Destructor.
+Elastic1DGap::~Elastic1DGap(){
+    //Does nothing.
+}
+
+//Clone the 'Elastic1DGap' material.
+std::unique_ptr<Material>
+Elastic1DGap::CopyMaterial(){
+    return std::make_unique<Elastic1DGap>(E, Gap, Behavior);
+}
+
+//Access material density.
+double 
+Elastic1DGap::GetDensity() const{
+    return 0.0;
+}
+
+//Returns the Poisson's ratio.
+double 
+Elastic1DGap::GetPoissonRatio() const{
+    return 0.0;
+}
+
+//Access bulk modulus.
+double 
+Elastic1DGap::GetBulkModulus() const{
+    return 0.0;
+}
+
+//Gets the linear Shear Modulus.
+double 
+Elastic1DGap:: GetShearModulus() const{
+    return 0.0;
+}
+
+//Access modulus of elasticity.
+double 
+Elastic1DGap::GetElasticityModulus() const{
+    return E;
+}
+
+//Access the material's energy at current strain.
+double 
+Elastic1DGap::GetEnergy() const{
+	//TODO: Compute the energy
+    double W = 0.0;
+    return W;
+}
+
+//Returns the material viscous damping.
+Eigen::MatrixXd 
+Elastic1DGap::GetDamping() const{
+    //Compute the damping.
+    Eigen::MatrixXd Damping(1,1);
+    Damping.fill(0.0); 
+
+    return Damping;
+}
+
+//Returns material strain vector.
+Eigen::VectorXd
+Elastic1DGap::GetStrain() const{
+    Eigen::VectorXd Strain(1);
+    Strain << oldStrain;
+    return Strain;
+}
+
+//Returns material stress vector.
+Eigen::VectorXd
+Elastic1DGap::GetStress() const{
+	Eigen::VectorXd Stress(1);
+    
+    if(Behavior){
+        Stress << E*(oldStrain - Gap)*(oldStrain > Gap);
+    }
+    else{
+        Stress << E*(oldStrain + Gap)*(oldStrain < -Gap);
+    }
+
+    return Stress;
+}
+
+//Returns material strain rate vector.
+Eigen::VectorXd 
+Elastic1DGap::GetStrainRate()  const{
+    //Compute the strain rate.
+    Eigen::VectorXd StrainRate(1);
+    StrainRate.fill(0.0); 
+
+    return StrainRate;
+}
+
+//Computes the material total stress.
+Eigen::VectorXd 
+Elastic1DGap::GetTotalStress() const{
+    return GetStress();
+}
+
+//Computes consistent material matrix.
+Eigen::MatrixXd
+Elastic1DGap::GetTangentStiffness() const{
+	Eigen::MatrixXd TangentStiffness(1,1);
+    TangentStiffness << 0.0;
+
+    if(Behavior){
+        TangentStiffness << E*(oldStrain > Gap);
+    }
+    else{
+        TangentStiffness << E*(oldStrain < -Gap);
+    }
+
+    return TangentStiffness;
+}
+
+//Returns the initial material stiffness.
+Eigen::MatrixXd 
+Elastic1DGap::GetInitialTangentStiffness() const{
+	Eigen::MatrixXd TangentStiffness(1,1);
+    TangentStiffness << E*(Gap < TOL);
+
+    return TangentStiffness;
+}
+
+//Perform converged material state update.
+void 
+Elastic1DGap::CommitState(){
+    //Does nothing
+}
+
+//Reverse the material states to previous converged state.
+void 
+Elastic1DGap::ReverseState(){
+    //Does nothing
+}
+
+//Brings the material states to its initial state in the element.
+void 
+Elastic1DGap::InitialState(){
+	oldStrain = 0.0;
+}
+
+//Update the material state for this iteration.
+void
+Elastic1DGap::UpdateState(const Eigen::VectorXd strain, unsigned int UNUSED(cond)){
+    //Update the strain and internal variables.
+	oldStrain = strain(0);
+}
---
+++ 02-Run_Process/02-Materials/02-NonLinear/Elastic1DGap.hpp
@@ -0,0 +1,153 @@
+//==============================================================================
+//
+//                    (Seismo)s (V)irtual (Lab)oratory
+//             Module for Serial and Parallel Analysis of seismic 
+//         wave propagation and soil-structure interaction simulation
+//         Copyright (C) 2018, The California Institute of Technology
+//                           All Rights Reserved.
+//
+// Commercial use of this program without express permission of the California
+// Institute of Technology, is strictly  prohibited. See  file "COPYRIGHT"  in
+// main  directory  for  information on  usage  and  redistribution, and for a
+// DISCLAIMER OF ALL WARRANTIES.
+//
+//==============================================================================
+//
+// Written by:
+//   Danilo S. Kusanovic (dkusanov@caltech.edu)
+//   Elnaz E. Seylabi    (elnaze@unr.edu)
+//
+// Supervised by:
+//   Domniki M. Asimaki  (domniki@caltech.edu)
+//
+// References : 
+//   [1]
+//
+// Description:
+///This file contains the "Elastic1DGap" material declarations, which defines 
+///an elastic gap material for 1D, 2D, or 3D analysis to be used along with
+///zero-length element.
+//------------------------------------------------------------------------------
+
+#ifndef _ELASTIC1DGAP_HPP_
+#define _ELASTIC1DGAP_HPP_
+
+#include "Material.hpp"
+
+/// @author    Danilo S. Kusanovic (dkusanov@caltech.edu)
+/// @date      April 26, 2021
+/// @version   1.0
+/// @file      Elastic1DGap.hpp
+/// @class     Elastic1DGap
+/// @see       Material.hpp
+/// @brief     Class for creating an uniaxial elastic gap material compatible with zero-length element
+class Elastic1DGap : public Material {
+
+    public:
+        ///Creates a Elastic1DGap material to be specified at a Gauss-point in an Element.
+        ///@param E The elastic moduli of this material
+        ///@param gap The separation from which the material start reacting
+        ///@param behavior The material behavior (true: Tension, false: Compression)
+        ///@see Elastic1DGap::E, Elastic1DGap::gap.
+        Elastic1DGap(double E, double gap, bool behavior=false);
+
+        ///Destroys this Elastic1DGap material.
+        ~Elastic1DGap();
+
+        //Clone the 'Elastic1DGap' material.
+        ///@return A Material pointer to the Elastic1DGap derived class.
+        ///@see Material, Elastic1DGap.
+        std::unique_ptr<Material> CopyMaterial();
+
+        ///Access material density.
+        ///@return The material density value.
+        double GetDensity() const;
+
+        ///Returns the Poisson's ratio.
+        ///@return The material poisson's ratio.
+        double GetPoissonRatio() const;
+
+        ///Access bulk modulus.
+        ///@return The material bulk's (K) modulus.
+        double GetBulkModulus() const;
+
+        ///Returns linear shear modulus.
+        ///@return The material density value.
+        double GetShearModulus() const;
+
+        ///Access modulus of elasticity.
+        ///@return The material Elasticity's (E) modulus.
+        ///@see Elastic1DGap::E.
+        double GetElasticityModulus() const;
+
+        ///Access the material's energy at current strain.
+        ///@return Scalar with the material energy value.
+        double GetEnergy() const;
+
+        ///Returns the material viscous damping.
+        ///@return Vector with the material damping components.
+        Eigen::MatrixXd GetDamping() const;
+
+        ///Returns the material strain.
+        ///@return Vector with the material strain components.
+        ///@note The strain can be revisited in @ref linkElastic1DGap.
+        ///@see Elastic1DGap::oldStrain.
+        Eigen::VectorXd GetStrain() const;
+
+        ///Returns the material stress. 
+        ///@return Vector with the material stress components.
+        ///@note The stress can be revisited in @ref linkElastic1DGap.
+        ///@see Elastic1DGap::oldStress.
+        Eigen::VectorXd GetStress() const;
+
+        ///Returns the material strain rate.
+        ///@return Vector with the material strain-rate components.
+        ///@note The stress-rate can be revisited in @ref linkElastic1DGap.
+        Eigen::VectorXd GetStrainRate() const;
+
+        ///Computes the material total stress.
+        ///@return Vector with the material total stress components.
+        ///@note The damping can be revisited in @ref linkElastic1DGap.
+        ///@see Elastic1DGap::oldStress.
+        Eigen::VectorXd GetTotalStress() const;
+
+        ///Returns the material stiffness.
+        ///@return Matrix with the material tangent stiffness matrix.
+        ///@note The stiffness matrix can be revisited in @ref linkElastic1DGap.
+        ///@see Elastic1DGap::oldTangentStiffness.
+        Eigen::MatrixXd GetTangentStiffness() const;
+
+        ///Returns the initial material stiffness.
+        ///@return Matrix with the initial material tangent stiffness matrix.
+        ///@note The initial tangent stiffness matrix is computed when the strain vector is zero.
+        Eigen::MatrixXd GetInitialTangentStiffness() const;
+
+        ///Perform converged material state update.
+        ///@note This function sets the trail stress and strain as converged.
+        void CommitState();
+
+        ///Reverse the material states to previous converged state.
+        ///@note This function returns the material states to previous converged states.
+        void ReverseState();
+
+        ///Brings the material states to its initial state in the element.
+        ///@note This function returns the material states to the beginning.
+        void InitialState();
+
+        ///Update the material state for this iteration.
+        ///@param strain Vector with the strain components at this Gauss-point.
+        ///@param cond If the the elastic/plastic material components will be updated.
+        ///@note This function computes the strain and tanget stiffness matrix once the trial strain converged.
+        void UpdateState(const Eigen::VectorXd strain, const unsigned int cond=0);
+
+    private:
+		//Elastic Gap properties: 
+		double E;
+		double Gap;
+		bool Behavior;
+
+		//Elastic Gap history variables:
+		double oldStrain;
+};
+
+#endif
