e8d673dfe8d7c48a16be8cca753fd7db4c2608a2
danilo <dankusanovic>
2021-06-18 15:18:27 -0700

    The SVLclasses dictionary now provides with the element group that it belongs to

--- 01-Pre_Process/Core/Definitions.py
+++ 01-Pre_Process/Core/Definitions.py
@@ -86,39 +86,30 @@ SVLclasses = {
     #  'Elements'  : Stores the elements classes defined in SVL
     'Materials': {
         'ELASTIC1DLINEAR': {
-            'color': [0,255,255], 
             'dim'  : [1,2,3]
         },
         'HERTZIAN1DLINEAR': {
-            'color': [29,249,20], 
             'dim': [1,2,3]
         },
         'VISCOUS1DLINEAR': {
-            'color': [255,0,255], 
             'dim': [1,2,3]
         },
         'PLASTIC1DJ2': {
-            'color': [146,110,174], 
             'dim': [1,2,3]
         },
         'ELASTIC2DPLANESTRAIN': {
-            'color': [0,255,0], 
             'dim': [2,3]
         },
         'ELASTIC2DPLANESTRESS': {
-            'color': [255,0,0], 
             'dim': [2,3]
         },
         'PLASTICPLANESTRAINJ2': {
-            'color': [59,176,143], 
             'dim': [2]
         },
         'PLASTICPLANESTRAINBA': {
-            'color': [165,105,79], 
             'dim': [2]
         },
         'ELASTIC3DLINEAR': {
-            'color': [0,0,255], 
             'dim': [3]
         },
         'PLASTIC3DJ2': {
@@ -126,69 +117,53 @@ SVLclasses = {
             'dim': [3]
         },
         'PLASTIC3DBA': {
-            'color': [255,182, 83], 
             'dim': [3]
         },
         'ELASTIC1DFIBER': {
-            'color': [153,76,0], 
             'dim': [1,2,3]
         },
         'STEEL1DFIBER': {
-            'color': [0,153,0], 
             'dim': [1,2,3]
         },
         'CONCRETE1DFIBER': {
-            'color': [128,128, 128], 
             'dim': [1,2,3]
         },
         'ELASTIC1DGAP': {
-            'color': [183, 149, 11], 
             'dim': [1,2,3]
         },
         'PLASTIC1DGAP': {
-            'color': [44, 62, 80], 
             'dim': [1,2,3]
         }
     },
     'Sections': {
         'LIN2DANGLE': {
-            'color': [0,255,255], 
             'dim': [2]
         },
         'LIN2DCHANNEL': {
-            'color': [29,249,20], 
             'dim': [2]
         },
         'LIN2DCIRCULAR': {
-            'color': [255,0,255], 
             'dim': [2]
         },
         'LIN2DCIRCULARTUBE': {
-            'color': [146,110,174], 
             'dim': [2]
         },
         'LIN2DRECTANGULAR': {
-            'color': [255,0,0], 
             'dim': [2]
         },
         'LIN2DRECTANGULARTUBE' : {
-            'color': [0,255,0], 
             'dim': [2]
         },
         'LIN2DTEE': {
-            'color': [165,105,79], 
             'dim': [2]
         },
         'LIN2DWIDEFLANGE': {
-            'color': [255,182,83], 
             'dim': [2]
         },
         'LIN3DANGLE': {
-            'color': [0,255,255], 
             'dim': [3]
         },
         'LIN3DCHANNEL': {
-            'color': [29,249,20], 
             'dim': [3]
         },
         'LIN3DCIRCULAR': {
@@ -196,239 +171,232 @@ SVLclasses = {
             'dim': [3]
         },
         'LIN3DCIRCULARTUBE': {
-            'color': [146,110,174], 
             'dim': [3]
         },
         'LIN3DRECTANGULAR': {
-            'color': [255,0,0], 
             'dim': [3]
         },
         'LIN3DRECTANGULARTUBE': {
-            'color': [0,255,0], 
             'dim': [3]
         },
         'LIN3DTEE': {
-            'color': [165,105,79], 
             'dim': [3]
         },
         'LIN3DTHINAREA': {
-            'color': [0,0,255], 
             'dim': [3]
         },
         'LIN3DWIDEFLANGE': {
-            'color': [255,182,83], 
             'dim': [3]
         },
         'FIB3DLINESECTION': {
-            'color': [255,0,255], 
             'dim': [3]
         },
         'FIB3DAREASECTION': {
-            'color': [221,160,221], 
             'dim': [3]
         },
     },
     'Elements': {
         'ZEROLENGTH1D': {
-            'type': 'LINE' , 
-            'color': [29,249, 20], 
+            'type': 'LINE',
+            'group': 31,
             'paraview': 3, 
             'dim': [1,2,3]
         },
         'LIN2DTRUSS2': {
             'type': 'LINE', 
-            'color': [25,25,25], 
+            'group': 10,
             'paraview': 3, 
             'dim': [2]
         },
         'KIN2DTRUSS2': {
-            'type': 'LINE', 
-            'color': [25,25,25], 
+            'type': 'LINE',
+            'group': 10,
             'paraview': 3, 
             'dim': [2]
         },
         'LIN3DTRUSS2': {
             'type': 'LINE' , 
-            'color': [25,25,25], 
+            'group': 10,
             'paraview': 3, 
             'dim': [3]
         },
         'KIN3DTRUSS2': {
             'type': 'LINE', 
-            'color': [25,25,25], 
+            'group': 10,
             'paraview': 3, 
             'dim': [3]
         },
         'LIN2DTRUSS3': {
             'type': 'LINE', 
-            'color': [ 25, 25, 25], 
-            'paraview': 4, 
+            'group': 10,
+            'paraview': 21, 
             'dim': [2]
         },
         'LIN3DTRUSS3': {
             'type': 'LINE', 
-            'color': [25,25,25], 
-            'paraview': 4, 
+            'group': 10,
+            'paraview': 21, 
             'dim': [3]
         },
         'LIN2DTRIA3': {
             'type': 'TRIA' , 
-            'color': [146,110,174], 
-            'paraview': 4, 
+            'group': 11,
+            'paraview': 5, 
             'dim': [2]
         },
         'LIN2DTRIA6': {
             'type': 'TRIA' , 
-            'color': [146,110,174], 
-            'paraview': 7, 
+            'group': 11,
+            'paraview': 22, 
             'dim': [2]
         },
         'LIN2DQUAD4': {
             'type': 'QUAD', 
-            'color': [ 59,176,143], 
-            'paraview': 5, 
+            'group': 12,
+            'paraview': 9, 
             'dim': [2]
         },
         'KIN2DQUAD4': {
             'type': 'QUAD' , 
-            'color': [ 59,176,143], 
-            'paraview': 5, 
+            'group': 12,
+            'paraview': 9, 
             'dim': [2]
         },
         'LIN2DQUAD8': {
             'type': 'QUAD', 
-            'color': [ 59,176,143], 
-            'paraview': 9, 
+            'group': 12,
+            'paraview': 23, 
             'dim': [2]
         },
         'PML2DQUAD4': {
             'type': 'QUAD', 
-            'color': [255,182, 83], 
-            'paraview': 5, 
+            'group': 12,
+            'paraview': 9, 
             'dim': [2]
         },
         'PML2DQUAD8': {
             'type': 'QUAD', 
-            'color': [255,182, 83], 
-            'paraview': 9, 
+            'group': 32,
+            'paraview': 23, 
             'dim': [2]
         },
         'LIN2DFRAME2': {
             'type': 'LINE', 
-            'color': [165,105, 79], 
+            'group': 20,
             'paraview': 3, 
             'dim': [2]
         },
         'KIN2DFRAME2': {
             'type': 'LINE', 
-            'color': [165,105, 79], 
-            'paraview': 3, 'dim': [2]
+            'group': 20,
+            'paraview': 3, 
+            'dim': [2]
         },
         'LIN3DFRAME2': {
             'type': 'LINE', 
-            'color': [165,105, 79], 
+            'group': 20,
             'paraview': 3, 
             'dim': [3]
         },
         'LIN3DSHELL3': {
             'type': 'TRIA', 
-            'color': [128,218,235], 
-            'paraview': 4, 
+            'group': 21,
+            'paraview': 5, 
             'dim': [3]
         },
         'LIN3DSHELL4': {
             'type': 'QUAD', 
-            'color': [128,218,235], 
-            'paraview': 5, 
+            'group': 21,
+            'paraview': 9, 
             'dim': [3]
         },
         'LIN3DTETRA4': {
             'type': 'TETRA', 
-            'color': [255,155,170], 
-            'paraview': 5, 
+            'group': 13,
+            'paraview': 10, 
             'dim': [3]
         },
         'LIN3DTETRA10': {
             'type': 'TETRA', 
-            'color': [255,155,170], 
-            'paraview': 11, 
+            'group': 13,
+            'paraview': 24, 
             'dim': [3]
         },
         'LIN3DHEXA8': {
             'type': 'HEXA', 
-            'color': [255,255,159], 
-            'paraview': 9, 
+            'group': 14,
+            'paraview': 12, 
             'dim': [3]
         },
         'KIN3DHEXA8': {
             'type': 'HEXA', 
-            'color': [255,255,159], 
-            'paraview': 9, 
+            'group': 14,
+            'paraview': 12, 
             'dim': [3]
         },
         'LIN3DHEXA20': {
             'type': 'HEXA', 
-            'color': [255,255,159], 
-            'paraview': 21, 
+            'group': 14,
+            'paraview': 25, 
             'dim': [3]
         },
         'PML3DHEXA8': {
             'type': 'HEXA', 
-            'color': [176,183,198], 
-            'paraview': 9, 
+            'group': 14,
+            'paraview': 12, 
             'dim': [3]
         },
         'PML3DHEXA20': {
             'type': 'HEXA', 
-            'color': [176,183,198], 
-            'paraview': 21, 
+            'group': 14,
+            'paraview': 25, 
             'dim': [3]
         },
         'NULL2DFRAME2': {
             'type': 'LINE', 
-            'color': [29,249, 20], 
+            'group': 50,
             'paraview': 3, 
             'dim': [2]
         },
         'NULL3DFRAME2': {
             'type': 'LINE', 
-            'color': [29,249, 20], 
+            'group': 50,
             'paraview': 3, 
             'dim': [3]
         },
         'EQLIN2DQUAD4': {
             'type': 'QUAD', 
-            'color': [59,176,143], 
-            'paraview': 5, 
+            'group': 12,
+            'paraview': 9, 
             'dim': [2]
         },
         'TIEQLIN2DQUAD4': {
             'type': 'QUAD', 
-            'color': [59,176,143], 
-            'paraview': 5, 
+            'group': 12,
+            'paraview': 9, 
             'dim': [2]
         },
         'UNXBOUCWEN2DLINK': {
             'type': 'LINE', 
-            'color': [0,0,0], 
+            'group': 41,
             'paraview': 3, 
             'dim': [2]
         },
         'UNXBOUCWEN3DLINK': {
             'type': 'LINE', 
-            'color': [0,0,0], 
+            'group': 41,
             'paraview': 3, 
             'dim': [3]
         },
         'HDRBYAMAMOTO2DLINK': {
             'type': 'LINE', 
-            'color': [0,0,0], 
+            'group': 42,
             'paraview': 3, 
             'dim': [2]
         },
         'HDRBYAMAMOTO3DLINK': {
             'type': 'LINE', 
-            'color': [0,0,0], 
+            'group': 42, 
             'paraview': 3, 
             'dim': [3]
         }
