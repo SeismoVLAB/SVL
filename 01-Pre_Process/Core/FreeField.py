abcadfafaf5753b349fddfda21bb5600c87115db
Danilo <dankusanovic@gmail.com>
2021-11-18 10:01:53 -0800

    New comments were added to Rayeigh wave DRM generation

--- 01-Pre_Process/Core/FreeField.py
+++ 01-Pre_Process/Core/FreeField.py
@@ -38,7 +38,7 @@ def GetTimeSeries(vals, dt, option):
     
     return Disp, Vels, Accel
 
-def Compute2DFreeFieldBoundaries(x, y, vals, dt, xmin, layers, beta, rho, nu, option, df=0.2, cof=30.0):
+def Compute2DFreeFieldBoundaries(x, y, signal, dt, xmin, layers, beta, rho, nu, option, df=0.2, cof=30.0):
     '''
     This function calculates the free-field forces required to be applied at the boundaries.\n
     @visit  https://github.com/SeismoVLAB/SVL\n
@@ -47,10 +47,10 @@ def Compute2DFreeFieldBoundaries(x, y, vals, dt, xmin, layers, beta, rho, nu, op
     Parameters
     ----------
     x  : array
-        Vector with the x-coordinatesto compute the free-field forces
+        Vector with the x-coordinates to compute the free-field forces
     y  : array
-        Vector with the y-coordinatesto compute the free-field forces
-    vals  : array
+        Vector with the y-coordinates to compute the free-field forces
+    signal  : array
         Vector with the time series values from which the forces will be computed
     dt  : float
         The time step of the time-series
@@ -65,10 +65,10 @@ def Compute2DFreeFieldBoundaries(x, y, vals, dt, xmin, layers, beta, rho, nu, op
     fun = { 'option': 'SV', 'df': df, 'CutOffFrequency': cof}
 
     #Computes the input displacement, velocities and accelerations.
-    Disp, Vels, Accel = GetTimeSeries(vals, dt, option)
+    Disp, Vels, Accel = GetTimeSeries(signal, dt, option)
 
-    #Transform time-series in frequency domain and computes variables required to compute reponse layer interface
-    nt = len(vals)
+    #Transform time-series in frequency domain and computes variables required to compute reponses layer interface
+    nt = len(signal)
     ufull, vfull, afull, layers, beta, rho, nu, wVec, p, s, h, mu, aSP, phaseVelIn, sinTheta, N, Nt = DataPreprocessing(Disp, Vels, Accel, layers, beta, rho, nu, angle, y0, nt, dt, fun)
 
     #Compute Interface responses
@@ -91,5 +91,13 @@ def Compute2DFreeFieldBoundaries(x, y, vals, dt, xmin, layers, beta, rho, nu, op
         #Computes the dashpot coefficients associated to this coordinate
         print('running!!!')
 
-def Compute3DFreeFieldBoundaries(x, y, vals, dt, xmin, layers, beta, rho, nu, option, df=0.2, cof=30.0):
+def Compute3DFreeFieldBoundaries(x, y, signal, dt, xmin, layers, beta, rho, nu, option, df=0.2, cof=30.0):
+    '''
+    '''
     pass
+
+def Find2DBoundaries(xleft, xright, ybottom):
+    '''
+    '''
+    Boundaries = {'left': [], 'right': [], 'bottom': []}
+    return Boundaries
